5bd90d0017fee4c4078525ebf381006170deeabb
Matej Drobnič <matej.drobnic@inova.si>
2018-04-10 09:17:37 +0200

    refactor: upgrade support library to 7.1.1
    This upgrade caused Robolectric's fragment tests to break. That test was also refactored.

---
+++ kotlinova-testutils/src/main/java/android/support/v4/app/TestFragmentAccessors.kt
@@ -0,0 +1,23 @@
+/**
+ * @author Matej Drobnic
+ */
+
+package android.support.v4.app
+
+import android.content.Context
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+
+fun Fragment.setHost(host: FragmentHostCallback<*>) {
+    this.mHost = host
+}
+
+fun Fragment.setContext(context: Context) {
+    setHost(mock<FragmentHostCallback<Unit>>()
+            .apply { whenever(getContext()).thenReturn(context) })
+            .apply {
+                if (context is FragmentActivity) {
+                    whenever(activity).thenReturn(context)
+                }
+            }
+}
--- kotlinova-testutils/src/main/java/si/inova/kotlinova/testing/TestingFragmentFactory.kt
+++ kotlinova-testutils/src/main/java/si/inova/kotlinova/testing/TestingFragmentFactory.kt
@@ -1,11 +1,15 @@
 package si.inova.kotlinova.testing
 
+import android.content.Context
 import android.support.v4.app.Fragment
+import android.support.v4.app.setContext
 import org.junit.Assert
+import si.inova.kotlinova.ui.SubtitledComponent
+import si.inova.kotlinova.ui.TitledComponent
 
 /**
  * Fragment factory that creates dummy fragments and binds them to the types of the original fragments
- * So they can be identified
+ * So they can be identified.
  *
  * @author Matej Drobnic
  */
@@ -14,7 +18,16 @@ class TestingFragmentFactory : (() -> Fragment) -> Fragment {
 
     override fun invoke(createOriginal: () -> Fragment): Fragment {
         val originalFragment = createOriginal()
-        val newFragment = Fragment()
+
+        val newFragment = when (originalFragment) {
+            is SubtitledComponent -> TestSubTitledFragment().apply {
+                this.originalFragment = originalFragment
+            }
+            is TitledComponent -> TestTitledFragment().apply {
+                this.originalFragment = originalFragment
+            }
+            else -> Fragment()
+        }
 
         fragmentMap[originalFragment::class.java] = newFragment
         return newFragment
@@ -38,5 +51,35 @@ class TestingFragmentFactory : (() -> Fragment) -> Fragment {
                         "Supplied Fragment is not of type ${T::class.java.name}",
                         fragmentOfThatClass, fragment
                 )
+    }
+
+    class TestTitledFragment : Fragment(),
+            TitledComponent {
+        var originalFragment: TitledComponent? = null
+
+        override val title: String
+            get() = originalFragment?.title ?: ""
+
+        override fun onAttach(context: Context) {
+            (originalFragment as? Fragment)?.setContext(context)
+
+            super.onAttach(context)
+        }
+    }
+
+    class TestSubTitledFragment : Fragment(),
+            SubtitledComponent {
+        var originalFragment: SubtitledComponent? = null
+
+        override val title: String
+            get() = originalFragment?.title ?: ""
+        override val subtitle: String
+            get() = originalFragment?.subtitle ?: ""
+
+        override fun onAttach(context: Context) {
+            (originalFragment as? Fragment)?.setContext(context)
+
+            super.onAttach(context)
+        }
     }
 }
