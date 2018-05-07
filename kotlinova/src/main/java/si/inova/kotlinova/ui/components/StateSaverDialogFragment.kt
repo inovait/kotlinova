af55bd41157dc726ffd8171c69bdc72c14ec5910
Jan Grah <jan.grah@inova.si>
2018-05-07 11:00:02 +0200

    refactor: move ImageFileHelper to apps utils package, add StateSaverDialogFragment

---
+++ kotlinova/src/main/java/si/inova/kotlinova/ui/components/StateSaverDialogFragment.kt
@@ -0,0 +1,49 @@
+package si.inova.kotlinova.ui.components
+
+import android.os.Bundle
+import android.support.annotation.CallSuper
+import android.support.v4.app.DialogFragment
+import si.inova.kotlinova.ui.state.StateSaverManager
+import si.inova.kotlinova.ui.state.StateSavingComponent
+
+/**
+ * Dialog fragment that provides automatic state saving facilities out of the box
+ *
+ * @author Matej Drobnic
+ */
+open class StateSaverDialogFragment : DialogFragment(), StateSavingComponent {
+    override val stateSaverManager = StateSaverManager()
+    /**
+     * Whether this dialog fragment has just been created for the first time.
+     *
+     * *true* means dialog fragment has been created for the first time.
+     *
+     * *false* means dialog fragment has been recreated from the previous state
+     * (for example when being recreated from configuration change or from backstack)
+     */
+    var createdForTheFirstTime = true
+        private set
+
+    @CallSuper
+    override fun onCreate(savedInstanceState: Bundle?) {
+        if (savedInstanceState != null) {
+            stateSaverManager.restoreInstance(savedInstanceState)
+        }
+
+        super.onCreate(savedInstanceState)
+
+        createdForTheFirstTime = createdForTheFirstTime && savedInstanceState == null
+    }
+
+    @CallSuper
+    override fun onSaveInstanceState(outState: Bundle) {
+        stateSaverManager.saveInstance(outState)
+        super.onSaveInstanceState(outState)
+    }
+
+    @CallSuper
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        createdForTheFirstTime = false
+    }
+}
