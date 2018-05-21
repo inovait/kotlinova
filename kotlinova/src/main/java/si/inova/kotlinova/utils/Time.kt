1775dd241abb753defa2917ecc05f43dda87208d
Matej Drobnič <matej.drobnic@inova.si>
2018-05-21 13:26:13 +0200

    refactor: convert Workout local date to LocalDateTime

--- kotlinova/src/main/java/si/inova/kotlinova/utils/Time.kt
+++ kotlinova/src/main/java/si/inova/kotlinova/utils/Time.kt
@@ -3,6 +3,11 @@
 package si.inova.kotlinova.utils
 
 import android.content.Context
+import org.threeten.bp.chrono.IsoChronology
+import org.threeten.bp.format.DateTimeFormatter
+import org.threeten.bp.format.DateTimeFormatterBuilder
+import org.threeten.bp.format.ResolverStyle
+import org.threeten.bp.temporal.ChronoField
 import si.inova.kotlinova.R
 import java.util.Calendar
 import java.util.Date
@@ -42,6 +47,33 @@ object TimeFormat {
 fun Date.toCalendar(): Calendar {
     return Calendar.getInstance().apply { time = this@toCalendar }
 }
+
+/**
+ * Time formater similar to [DateTimeFormatter.ISO_LOCAL_TIME], except that it always
+ * prints out seconds, even if those are zeros. It also never prints out nanoseconds.
+ */
+val ISO_LOCAL_TIME_WITH_FORCED_SECONDS by lazy {
+    DateTimeFormatterBuilder()
+        .appendValue(ChronoField.HOUR_OF_DAY, 2)
+        .appendLiteral(':')
+        .appendValue(ChronoField.MINUTE_OF_HOUR, 2)
+        .appendLiteral(':')
+        .appendValue(ChronoField.SECOND_OF_MINUTE, 2)
+        .toFormatter()
+        .withResolverStyle(ResolverStyle.STRICT)
+}
+
+val ISO_LOCAL_DATE_TIME_WITH_FORCED_SECONDS by lazy {
+    DateTimeFormatterBuilder()
+        .parseCaseInsensitive()
+        .append(DateTimeFormatter.ISO_DATE)
+        .appendLiteral('T')
+        .append(ISO_LOCAL_TIME_WITH_FORCED_SECONDS)
+        .toFormatter()
+        .withResolverStyle(ResolverStyle.STRICT)
+        .withChronology(IsoChronology.INSTANCE)
+}
+
 
 const val ISO_8601_FORMAT_STRING = "yyyy-MM-dd'T'HH:mm:ssZ"
 const val ISO_8601_FORMAT_STRING_WITHOUT_TZ = "yyyy-MM-dd'T'HH:mm:ss"
