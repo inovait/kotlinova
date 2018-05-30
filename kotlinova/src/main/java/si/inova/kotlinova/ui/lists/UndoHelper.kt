9b1fbbd6228d1eade40359f0c2b775483521baf0
janko <kristjan.kotnik@inova.si>
2018-05-30 13:19:09 +0200

    refactor: add undoHelper into own class

---
+++ kotlinova/src/main/java/si/inova/kotlinova/ui/lists/UndoHelper.kt
@@ -0,0 +1,67 @@
+package si.inova.kotlinova.ui.lists
+
+import android.content.Context
+import android.os.Handler
+import android.support.design.widget.Snackbar
+import android.view.View
+import si.inova.kotlinova.R
+
+/**
+ * Class that handles undo for deleted items in lists
+ *
+ * On start init all callbacks for deleting
+ * To enable undo, use [performVisualDelete()][performVisualDelete] method.
+ *
+ * On method [onStop()] call [commitDeletionsAll()[commitDeletionsAll]
+ *
+ * @author Kristjan Kotnik
+ */
+class UndoHelper<T>(
+    private val context: Context,
+    private val rootView: View,
+    private val deleteText: String,
+    private val restoreText: String
+) {
+    var deleteCallback: ((T) -> Unit)? = null
+    var restoreCallBack: (() -> Unit)? = null
+    var notifyItemRemovedCallback: (() -> Unit)? = null
+    private val itemsToRemove = mutableListOf<T>()
+    private val handler = Handler()
+    private val runnable = Runnable { commitDeletionsAll() }
+
+    fun performVisualDelete(itemId: T) {
+        removeCallbacks()
+        itemsToRemove.add(itemId)
+        Snackbar
+            .make(rootView, deleteText, Snackbar.LENGTH_LONG)
+            .setAction(context.getString(R.string.undo)) {
+                commitDeletionsAllButLast()
+                Snackbar.make(rootView, restoreText, Snackbar.LENGTH_SHORT).show()
+            }.show()
+        notifyItemRemovedCallback?.invoke()
+        handler.postDelayed(runnable, SNACKBAR_DISPLAY_DELAY)
+    }
+
+    private fun removeCallbacks() {
+        handler.removeCallbacks(runnable)
+    }
+
+    fun commitDeletionsAll() {
+        for (i in 0 until itemsToRemove.count()) {
+            deleteCallback?.invoke(itemsToRemove[i])
+        }
+        restoreCallBack?.invoke()
+        itemsToRemove.clear()
+    }
+
+    private fun commitDeletionsAllButLast() {
+        for (i in 0 until itemsToRemove.count() - 1) {
+            deleteCallback?.invoke(itemsToRemove[i])
+        }
+        removeCallbacks()
+        restoreCallBack?.invoke()
+        itemsToRemove.clear()
+    }
+}
+
+private const val SNACKBAR_DISPLAY_DELAY = 3500L
