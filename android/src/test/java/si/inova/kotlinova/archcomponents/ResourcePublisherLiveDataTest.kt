86e3dc0f1bc85b4c9013bbf2ca8e9b008e5daecd
Matej Drobnič <matej.drobnic@inova.si>
2018-07-03 08:33:18 +0200

    test: add subscribe propagate test for ResourcePublisherLiveData

--- android/src/test/java/si/inova/kotlinova/archcomponents/ResourcePublisherLiveDataTest.kt
+++ android/src/test/java/si/inova/kotlinova/archcomponents/ResourcePublisherLiveDataTest.kt
@@ -1,10 +1,21 @@
 package si.inova.kotlinova.archcomponents
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.lifecycle.Observer
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doAnswer
+import com.nhaarman.mockitokotlin2.inOrder
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.never
+import com.nhaarman.mockitokotlin2.whenever
 import io.reactivex.Flowable
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.Answers
+import org.reactivestreams.Publisher
+import org.reactivestreams.Subscriber
+import org.reactivestreams.Subscription
 import si.inova.kotlinova.data.resources.Resource
 import si.inova.kotlinova.testing.ImmediateDispatcherRule
 
@@ -36,5 +47,29 @@ class ResourcePublisherLiveDataTest {
         liveData.observeForever { }
 
         assertEquals(Resource.Error<Int>(error), liveData.value)
+    }
+
+    @Test
+    fun propagateSubscribeUnsubscribe() {
+        val publisher: Publisher<Resource<Int>> = mock(defaultAnswer = Answers.RETURNS_DEEP_STUBS)
+        val subscription: Subscription = mock()
+
+        doAnswer {
+            val subscriber: Subscriber<Resource<Int>> = it.getArgument(0)
+            subscriber.onSubscribe(subscription)
+        }.whenever(publisher).subscribe(any())
+
+        inOrder(publisher, subscription) {
+            val liveData = ResourcePublisherLiveData(publisher)
+            verify(publisher, never()).subscribe(any())
+
+            val observer: Observer<Resource<Int>> = mock()
+            liveData.observeForever(observer)
+            verify(publisher).subscribe(any())
+            verify(subscription, never()).cancel()
+
+            liveData.removeObserver(observer)
+            verify(subscription).cancel()
+        }
     }
 }
