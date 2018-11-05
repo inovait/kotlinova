41d598842a6506d8b2e9b1523eb09742140d3bbe
Matej Drobnič <matej.drobnic@inova.si>
2018-11-05 14:19:49 +0100

    refactor: android dispatcher returns CoroutineContext
    BREAKING CHANGE
    
    this is part of the WIP process to fully migrate kotlinova to Kotlin 1.3 with stable coroutines
    
    Signed-off-by: Matej Drobnič <matej.drobnic@inova.si>

--- android/src/main/java/si/inova/kotlinova/coroutines/AndroidDispatchers.kt
+++ android/src/main/java/si/inova/kotlinova/coroutines/AndroidDispatchers.kt
@@ -1,7 +1,7 @@
 package si.inova.kotlinova.coroutines
 
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlin.coroutines.CoroutineContext
 
 /**
  * Collection of Android Coroutine dispatchers that can be overriden for Unit tests.
@@ -12,7 +12,7 @@ import kotlinx.coroutines.Dispatchers
 /**
  * [kotlinx.coroutines.experimental.android.UI] dispatcher that can be overriden by unit tests.
  */
-val UI: CoroutineDispatcher
+val UI: CoroutineContext
     get() = dispatcherOverride {
         Dispatchers.Main
     }
