5b10ff53701d2acf787fc408c130a2bc07cfd994
Matej Drobnič <matej.drobnic@inova.si>
2018-11-07 07:34:08 +0100

    refactor: delete unused CenterFirstDecoration
    BREAKING CHANGE
    
    Signed-off-by: Matej Drobnič <matej.drobnic@inova.si>

--- android/src/main/java/si/inova/kotlinova/ui/lists/CenterFirstDecoration.kt
+++
@@ -1,8 +0,0 @@
-package si.inova.kotlinova.ui.lists
-
-import android.support.v7.widget.RecyclerView
-
-/**
- * @author Matej Drobnic
- */
-class CenterFirstDecoration : RecyclerView.ItemDecoration()
