1e1b9921d4f7870b1d0784a393dc8c4ac0b33db3
Matej Drobnič <matej.drobnic@inova.si>
2019-04-03 13:19:32 +0200

    feat(core): add more AssertJ utilities
    Signed-off-by: Matej Drobnič <matej.drobnic@inova.si>

--- core/src/main/java/si/inova/kotlinova/testing/asserts/AssertUtils.kt
+++ core/src/main/java/si/inova/kotlinova/testing/asserts/AssertUtils.kt
@@ -3,6 +3,7 @@
 package si.inova.kotlinova.testing.asserts
 
 import org.assertj.core.api.AbstractAssert
+import org.assertj.core.api.Condition
 
 /**
  * Variant of [AbstractAssert.matches] with flipped parameters to move lambda
@@ -13,4 +14,51 @@ fun <S : AbstractAssert<S, T>, T> AbstractAssert<S, T>.matches(
     predicate: (T) -> Boolean
 ): S {
     return matches(predicate, description)
+}
+
+fun <T> matching(description: String? = null, block: (T) -> Boolean): Condition<T> {
+    return object : Condition<T>(description) {
+        override fun matches(value: T): Boolean {
+            return block(value)
+        }
+    }
+}
+
+/**
+ * Extract value from assert. This should only be used to implement custom assertion, not with
+ * actual tests.
+ */
+val <T> AbstractAssert<*, T>.actualValue: T
+    get() {
+        var value: T? = null
+
+        satisfies {
+            value = it
+        }
+
+        return value!!
+    }
+
+/**
+ * Wait for specific test condition to be true. This test method makes the assertion periodically
+ * until it becomes true or until timeout hits.
+ *
+ * If you are running instrumented tests, it is generally recommended
+ * to use [IdlingResource] instead if possible
+ */
+fun waitForMatch(timeoutMs: Long = 5000, periodMs: Long = 10, condition: () -> Unit) {
+    val end = System.currentTimeMillis() + timeoutMs
+
+    while (true) {
+        try {
+            condition()
+            break
+        } catch (e: AssertionError) {
+            if (System.currentTimeMillis() >= end) {
+                throw e
+            }
+
+            Thread.sleep(periodMs)
+        }
+    }
 }
