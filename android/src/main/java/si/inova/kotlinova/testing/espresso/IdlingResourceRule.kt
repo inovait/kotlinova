e820cc55db265408d396138eafe6565cb85e1420
Matej Drobnič <matej.drobnic@inova.si>
2019-04-03 13:18:22 +0200

    feat(android): add per-test ignore notification to IdlingResourceRule
    Signed-off-by: Matej Drobnič <matej.drobnic@inova.si>

--- android/src/main/java/si/inova/kotlinova/testing/espresso/IdlingResourceRule.kt
+++ android/src/main/java/si/inova/kotlinova/testing/espresso/IdlingResourceRule.kt
@@ -17,9 +17,14 @@ import androidx.test.runner.lifecycle.ActivityLifecycleCallback
 import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
+import kotlin.reflect.KClass
 
 class IdlingResourceRule(private val resource: IdlingResource) : TestWatcher() {
-    override fun starting(description: Description?) {
+    override fun starting(description: Description) {
+        if (hasIgnoringAnnotation(description)) {
+            return
+        }
+
         IdlingRegistry.getInstance().register(resource)
 
         if (resource is ActivityLifecycleCallback) {
@@ -31,7 +36,7 @@ class IdlingResourceRule(private val resource: IdlingResource) : TestWatcher() {
         super.starting(description)
     }
 
-    override fun finished(description: Description?) {
+    override fun finished(description: Description) {
         super.finished(description)
 
         IdlingRegistry.getInstance().unregister(resource)
@@ -42,4 +47,22 @@ class IdlingResourceRule(private val resource: IdlingResource) : TestWatcher() {
                 .removeLifecycleCallback(resource)
         }
     }
+
+    private fun hasIgnoringAnnotation(description: Description): Boolean {
+        val annotation = description.getAnnotation(IgnoreIdlingResource::class.java) ?: return false
+
+        return annotation.types.contains(resource::class)
+    }
 }
+
+/**
+ * Ignore specific idling resource on this test.
+ *
+ * This is useful when test methods tests something that would espresso otherwise wait to be over,
+ * for example checking for progress bars.
+ *
+ * This annotation only has effect when idling resources are created with [IdlingResourceRuleEx]
+ * rule.
+ */
+@Target(AnnotationTarget.FUNCTION)
+annotation class IgnoreIdlingResource(vararg val types: KClass<out IdlingResource>)
