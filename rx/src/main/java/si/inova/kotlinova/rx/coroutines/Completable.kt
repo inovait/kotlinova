a8bcdd12ad1c6d798cccbe645b7d7a4a4c277eb4
Matej Drobnič <matej.drobnic@inova.si>
2019-04-03 13:21:48 +0200

    refactor: remove ConnectedTravel copyright
    This was leftover error from copy paste
    
    Signed-off-by: Matej Drobnič <matej.drobnic@inova.si>

--- rx/src/main/java/si/inova/kotlinova/rx/coroutines/Completable.kt
+++ rx/src/main/java/si/inova/kotlinova/rx/coroutines/Completable.kt
@@ -1,14 +1,3 @@
-/*
- * Copyright © 2016, Connected Travel, LLC – All Rights Reserved.
- *
- * All information contained herein is property of Connected Travel, LLC including, but
- * not limited to, technical and intellectual concepts which may be embodied within.
- *
- * Dissemination or reproduction of this material is strictly forbidden unless prior written
- * permission, via license, is obtained from Connected Travel, LLC.   If permission is obtained,
- * this notice, and any other such legal notices, must remain unaltered.
- */
-
 @file:JvmName("CompletableCoroutines")
 
 package si.inova.kotlinova.rx.coroutines
