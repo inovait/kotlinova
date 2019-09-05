d67916068ea139e8f52bb73d7eaa447cabc3151d
Matej Drobnič <matej.drobnic@inova.si>
2019-09-05 08:09:00 +0200

    feat(android): add a way to launch coroutines in Lifecycle scope
    Signed-off-by: Matej Drobnič <matej.drobnic@inova.si>

---
+++ android/src/main/java/si/inova/kotlinova/archcomponents/LifecycleCoroutines.kt
@@ -0,0 +1,40 @@
+package si.inova.kotlinova.archcomponents
+
+import androidx.lifecycle.GenericLifecycleObserver
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import si.inova.kotlinova.coroutines.TestableDispatchers
+import java.util.*
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+
+private val lifecycleJobs = WeakHashMap<Lifecycle, Job>()
+
+private fun Lifecycle.createJob(cancelEvent: Lifecycle.Event = Lifecycle.Event.ON_DESTROY): Job =
+        Job().also { job ->
+            addObserver(object : GenericLifecycleObserver {
+                override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+                    if (event == cancelEvent) {
+                        removeObserver(this)
+                        job.cancel()
+                    }
+                }
+            })
+        }
+
+val Lifecycle.job: Job
+    get() = lifecycleJobs[this] ?: createJob().also {
+        lifecycleJobs[this] = it
+        it.invokeOnCompletion { _ -> lifecycleJobs -= this }
+    }
+
+fun LifecycleOwner.launch(
+        context: CoroutineContext = EmptyCoroutineContext,
+        block: suspend CoroutineScope.() -> Unit
+) {
+    GlobalScope.launch(TestableDispatchers.Main + lifecycle.job + context, block = block)
+}
