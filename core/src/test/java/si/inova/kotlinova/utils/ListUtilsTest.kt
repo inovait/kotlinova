e0b2097f4309b20e440aa8dfbcd533365ade8064
Matej Drobnič <matej.drobnic@inova.si>
2019-11-12 12:48:17 +0100

    feat(core): add List.isNotNullOrEmpty
    Signed-off-by: Matej Drobnič <matej.drobnic@inova.si>

--- core/src/test/java/si/inova/kotlinova/utils/ListUtilsTest.kt
+++ core/src/test/java/si/inova/kotlinova/utils/ListUtilsTest.kt
@@ -1,7 +1,9 @@
 package si.inova.kotlinova.utils
 
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
 import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 /**
@@ -17,6 +19,15 @@ class ListUtilsTest {
             Holder(5)
         ).meanBy { it.number })
         assertEquals(3, listOf(1, 2, 3, 4, 5, 6).meanBy { it })
+    }
+
+    @Test
+    fun isNotNullOrEmpty() {
+        assertFalse((null as List<String>?).isNotNullOrEmpty())
+        assertFalse(emptyList<String>().isNotNullOrEmpty())
+        assertTrue(listOf("A", "B", "C").isNotNullOrEmpty())
+        assertTrue(listOf("A").isNotNullOrEmpty())
+        assertTrue(listOf("A").isNotNullOrEmpty())
     }
 
     private class Holder(val number: Int)
