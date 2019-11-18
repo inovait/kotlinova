5c4b2b1e30c2b90c80735862d7da6540edbc2867
Matej Drobnič <matej.drobnic@inova.si>
2019-11-18 15:14:42 +0100

    test(android): migrate ThreadUtilsTest from robolectric to instrumented test
    Signed-off-by: Matej Drobnič <matej.drobnic@inova.si>

--- android/src/androidTest/java/si/inova/kotlinova/utils/ThreadUtilsTest.kt
+++ android/src/androidTest/java/si/inova/kotlinova/utils/ThreadUtilsTest.kt
@@ -1,72 +1,60 @@
 package si.inova.kotlinova.utils
 
-import com.nhaarman.mockitokotlin2.inOrder
+import android.os.Looper
+import androidx.test.espresso.Espresso
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.verifyZeroInteractions
 import kotlinx.coroutines.Job
-import org.junit.Ignore
+import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.Robolectric
-import org.robolectric.RobolectricTestRunner
 import si.inova.kotlinova.testing.LocalFunction0
 import si.inova.kotlinova.testing.UncaughtExceptionThrowRule
 import kotlin.concurrent.thread
 
-/**
- * @author Matej Drobnic
- */
-@Ignore
-@RunWith(RobolectricTestRunner::class)
 class ThreadUtilsTest {
     @get:Rule
     val errorRule = UncaughtExceptionThrowRule()
 
     @Test
     fun runOnUiThreadWhenOnUiThread() {
-        Robolectric.getForegroundThreadScheduler().pause()
-
         val task: LocalFunction0<Unit> = mock()
 
-        runOnUiThread(block = task)
+        runOnUiThread {
+            assertTrue(Looper.myLooper() == Looper.getMainLooper())
+            task()
+        }
 
         verify(task).invoke()
     }
 
     @Test
     fun runOnUiThreadWhenOnSeparateThread() {
-        Robolectric.getForegroundThreadScheduler().pause()
         val task: LocalFunction0<Unit> = mock()
 
         thread {
-            runOnUiThread(block = task)
+            runOnUiThread {
+                assertTrue(Looper.myLooper() == Looper.getMainLooper())
+                task()
+            }
         }.join()
 
-        inOrder(task) {
-            verifyNoMoreInteractions()
-
-            Robolectric.getForegroundThreadScheduler().unPause()
-
-            verify(task).invoke()
-        }
+        verify(task).invoke()
     }
 
     @Test
     fun cancelRunOnUiThreadWhenOnSeparateThread() {
-        Robolectric.getForegroundThreadScheduler().pause()
         val task: LocalFunction0<Unit> = mock()
 
         val job = Job()
+        job.cancel()
 
         thread {
             runOnUiThread(parentJob = job, block = task)
         }.join()
 
-        job.cancel()
-
-        Robolectric.getForegroundThreadScheduler().unPause()
+        Espresso.onIdle()
 
         verifyZeroInteractions(task)
     }
