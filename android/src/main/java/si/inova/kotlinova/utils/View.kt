762440982ff7160c5bd78148a048b8b7173a9466
janko <kristjan.kotnik@inova.si>
2020-08-04 10:19:36 +0200

    feat(android): add showWithFade, hideWithFade and setVisibilityWithFade methods

--- android/src/main/java/si/inova/kotlinova/utils/View.kt
+++ android/src/main/java/si/inova/kotlinova/utils/View.kt
@@ -37,24 +37,42 @@ import androidx.fragment.app.Fragment
 var View.isVisibleWithFade: Boolean
     get() = isVisible
     set(value) {
-        val targetAlpha = if (value) 1f else 0f
-
-        if (!isVisible && value && alpha == 1f) {
-            alpha = 0f
-        }
-
-        isVisible = true
-
-        animate()
-            .alpha(targetAlpha)
-            .apply {
-                if (!value) {
-                    withEndAction { isVisible = false }
-                }
-            }
-            .setDuration(250)
-            .start()
+        setVisibilityWithFade(250L, value)
     }
+
+/**
+ * Variant of [isVisibleWithFade] with duration option
+ */
+fun View.setVisibilityWithFade(duration: Long, visible: Boolean) =
+    if (visible) showWithFade(duration) else hideWithFade(duration)
+
+/**
+ * Fades view in
+ */
+fun View.showWithFade(duration: Long) {
+    val targetAlpha = 1f
+
+    if (!isVisible && alpha == 1f) {
+        alpha = 0f
+    }
+
+    isVisible = true
+
+    animate().alpha(targetAlpha).setDuration(duration).start()
+}
+
+/**
+ * Fades view out
+ */
+fun View.hideWithFade(duration: Long) {
+    val targetAlpha = 0f
+
+    animate()
+        .alpha(targetAlpha)
+        .apply { withEndAction { isVisible = false } }
+        .setDuration(duration)
+        .start()
+}
 
 /**
  * Helper method to inflate given layout asynchronously
