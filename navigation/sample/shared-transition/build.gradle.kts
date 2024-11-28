e5abdf233f99fd540d354b53051b671885e0505b
Matej Drobnic <matej.drobnic@inova.si>
2024-11-28 13:21:16 +0100

    docs(navigation): add missing files

---
+++ navigation/sample/shared-transition/build.gradle.kts
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2024 INOVA IT d.o.o.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
+ * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+ *  is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ *   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+plugins {
+   id("com.android.library")
+   id("org.jetbrains.kotlin.android")
+   id("com.google.devtools.ksp")
+   id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+   namespace = "si.inova.kotlinova.navigation.sharedtransition"
+   compileSdk = 35
+
+   defaultConfig {
+      minSdk = 24
+      targetSdk = 34
+   }
+
+   compileOptions {
+      sourceCompatibility = JavaVersion.VERSION_17
+      targetCompatibility = JavaVersion.VERSION_17
+   }
+}
+
+dependencies {
+   implementation(projects.keys)
+
+   implementation(libs.androidx.compose.ui)
+   implementation(libs.androidx.compose.ui.graphics)
+   implementation(libs.androidx.compose.ui.tooling.preview)
+   implementation(libs.androidx.compose.material3)
+   implementation(libs.kotlinova.navigation)
+   implementation(libs.kotlinInject.runtime)
+   implementation(libs.kotlinInject.anvil.runtime)
+
+   ksp(libs.kotlinInject.compiler)
+   ksp(libs.kotlinInject.anvil.compiler)
+   ksp(libs.kotlinova.navigation.compiler)
+
+   debugImplementation(libs.androidx.compose.ui.test.manifest)
+   debugImplementation(libs.androidx.compose.ui.tooling)
+}
---
+++ navigation/sample/shared-transition/src/main/java/si/inova/kotlinova/navigation/sample/sharedtransition/SharedTransitionDeepLinkHandler.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2024 INOVA IT d.o.o.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
+ * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+ *  is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ *   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package si.inova.kotlinova.navigation.sample.sharedtransition
+
+import android.net.Uri
+import me.tatarka.inject.annotations.Inject
+import si.inova.kotlinova.navigation.deeplink.DeepLinkHandler
+import si.inova.kotlinova.navigation.deeplink.handleMultipleDeepLinks
+import si.inova.kotlinova.navigation.di.OuterNavigationScope
+import si.inova.kotlinova.navigation.instructions.NavigationInstruction
+import si.inova.kotlinova.navigation.instructions.ReplaceBackstackOrOpenScreen
+import si.inova.kotlinova.navigation.sample.keys.MainScreenKey
+import si.inova.kotlinova.navigation.sample.keys.SharedTransitionDetailsScreenKey
+import si.inova.kotlinova.navigation.sample.keys.SharedTransitionListScreenKey
+import software.amazon.lastmile.kotlin.inject.anvil.ContributesBinding
+
+@ContributesBinding(OuterNavigationScope::class, multibinding = true)
+class SharedTransitionDeepLinkHandler @Inject constructor() : DeepLinkHandler {
+   override fun handleDeepLink(uri: Uri, startup: Boolean): NavigationInstruction? {
+      return handleMultipleDeepLinks(uri, startup) {
+         matchDeepLink("navigationdemo://colors") { _, startup ->
+            ReplaceBackstackOrOpenScreen(startup, MainScreenKey, SharedTransitionListScreenKey)
+         }
+         matchDeepLink("navigationdemo://colors/{color}") { args, startup ->
+            val colorIndex = args.getValue("color").toIntOrNull()
+            if (colorIndex != null && colorIndex in SHARED_TRANSITION_COLORS.indices) {
+               ReplaceBackstackOrOpenScreen(startup, MainScreenKey, SharedTransitionListScreenKey, SharedTransitionDetailsScreenKey(colorIndex))
+            } else {
+               null
+            }
+         }
+      }
+   }
+}
