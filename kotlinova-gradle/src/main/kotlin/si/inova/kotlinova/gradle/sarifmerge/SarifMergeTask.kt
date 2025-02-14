8edc19a996d365730da8238b3e1982b17b5a0a71
Matej Drobnic <matej.drobnic@inova.si>
2025-02-14 07:17:01 +0100

    refactor(kotlinova-gradle): use sarif merging from sarif4k

--- kotlinova-gradle/src/main/kotlin/si/inova/kotlinova/gradle/sarifmerge/SarifMergeTask.kt
+++ kotlinova-gradle/src/main/kotlin/si/inova/kotlinova/gradle/sarifmerge/SarifMergeTask.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 INOVA IT d.o.o.
+ * Copyright 2025 INOVA IT d.o.o.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
@@ -17,11 +17,9 @@
 package si.inova.kotlinova.gradle.sarifmerge
 
 import io.github.detekt.sarif4k.Level
-import io.github.detekt.sarif4k.PropertyBag
 import io.github.detekt.sarif4k.ReportingConfiguration
-import io.github.detekt.sarif4k.Run
-import io.github.detekt.sarif4k.SarifSchema210
 import io.github.detekt.sarif4k.SarifSerializer
+import io.github.detekt.sarif4k.merge
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.RegularFileProperty
@@ -56,7 +54,7 @@ abstract class SarifMergeTask : DefaultTask() {
          return
       }
 
-      val merged = sarifFiles.reduce(SarifSchema210::merge)
+      val merged = sarifFiles.reduce { a, b -> a.merge(b) }
 
       val fixedSeverity = merged.copy(
          runs = merged.runs.map { run ->
@@ -85,62 +83,4 @@ abstract class SarifMergeTask : DefaultTask() {
 
       output.get().asFile.writeText(SarifSerializer.toJson(fixedSeverity))
    }
-}
-
-private fun SarifSchema210.merge(other: SarifSchema210): SarifSchema210 {
-   require(schema == other.schema) {
-      "Cannot merge sarifs with different schemas: '${schema ?: "null"}' or '${other.schema ?: "null"}'"
-   }
-   require(version == other.version) {
-      "Cannot merge sarifs with different versions: '$version' or '${other.version}'"
-   }
-
-   val mergedExternalProperties = (inlineExternalProperties.orEmpty() + other.inlineExternalProperties.orEmpty())
-      .takeIf { it.isNotEmpty() }
-
-   val mergedProperties = properties?.merge(other.properties)
-
-   val mergedRuns = runs.merge(other.runs)
-
-   return SarifSchema210(
-      schema,
-      version,
-      mergedExternalProperties,
-      mergedProperties,
-      mergedRuns
-   )
-}
-
-// Temporary copy from https://github.com/detekt/sarif4k/pull/87 until new version of sarif4k with that included is released
-
-private fun PropertyBag.merge(other: PropertyBag?): PropertyBag {
-   val aTags = this["tags"] as? Collection<*>
-   val bTags = (other?.get("tags") as? Collection<*>).orEmpty()
-   val tags = if (aTags != null) {
-      mapOf("tags" to (aTags + bTags).distinct())
-   } else {
-      emptyMap()
-   }
-
-   return PropertyBag(this + other.orEmpty() + tags)
-}
-
-private fun List<Run>.merge(other: List<Run>): List<Run> {
-   val runsByTool = (this + other).groupBy { it.tool.driver.fullName }
-
-   return runsByTool.mapValues { (_, runs) ->
-      val baseRun = runs.firstOrNull() ?: return@mapValues null
-
-      val mergedResults = runs.flatMap { it.results.orEmpty() }
-      val mergedRules = runs.flatMap { it.tool.driver.rules.orEmpty() }.distinctBy { it.id }
-
-      baseRun.copy(
-         results = mergedResults,
-         tool = baseRun.tool.copy(
-            driver = baseRun.tool.driver.copy(
-               rules = mergedRules
-            )
-         )
-      )
-   }.values.filterNotNull().toList()
 }
