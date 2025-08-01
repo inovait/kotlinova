9c40eaf091daed15d43f24422817ad84e4b55b7c
Matej Drobnic <matej.drobnic@inova.si>
2025-08-01 10:02:04 +0200

    fix(kotlinova-gradle): project isolation compatiblity for detect pre-commit hook
    findProperty does not work in project isolation: https://github.com/gradle/gradle/issues/28857

--- kotlinova-gradle/src/main/kotlin/si/inova/kotlinova/gradle/detektprecommit/DetectPreCommitHook.kt
+++ kotlinova-gradle/src/main/kotlin/si/inova/kotlinova/gradle/detektprecommit/DetectPreCommitHook.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 INOVA IT d.o.o.
+ * Copyright 2025 INOVA IT d.o.o.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
@@ -28,7 +28,7 @@ internal fun Project.registerDetektPreCommitHook(extension: KotlinovaExtension) 
          tasks.withType(Detekt::class.java).configureEach { detektTask ->
             detektTask.usesService(serviceProvider)
 
-            if (project.hasProperty("precommit")) {
+            if (project.providers.gradleProperty("precommit").orNull != null) {
                val fileCollection = files()
 
                val originalSource = detektTask.source
