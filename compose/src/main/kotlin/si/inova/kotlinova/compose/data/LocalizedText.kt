88c5999a4459419f04509a9a63b429e9815226e5
Matej Drobnic <matej.drobnic@inova.si>
2025-09-01 07:00:37 +0200

    chore: bump dependencies

--- compose/src/main/kotlin/si/inova/kotlinova/compose/data/LocalizedText.kt
+++ compose/src/main/kotlin/si/inova/kotlinova/compose/data/LocalizedText.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 INOVA IT d.o.o.
+ * Copyright 2025 INOVA IT d.o.o.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
@@ -17,13 +17,12 @@
 package si.inova.kotlinova.compose.data
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalResources
 import si.inova.kotlinova.core.data.LocalizedText
 
 @Composable
 fun LocalizedText.get(): CharSequence {
-   LocalConfiguration.current
+   LocalResources.current
 
-   return get(LocalContext.current.resources)
+   return get(LocalResources.current)
 }
