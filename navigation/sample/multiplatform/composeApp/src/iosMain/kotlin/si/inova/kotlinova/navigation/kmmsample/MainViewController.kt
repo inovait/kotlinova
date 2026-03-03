ef64eebc0211c7bfea01f3d05edda6a0a8a58189
Zvonimir Kemperle <zvonimir.kemperle@inova.si>
2026-03-03 14:13:42 +0100

    docs(navigation): fix iOSsample

--- navigation/sample/multiplatform/composeApp/src/iosMain/kotlin/si/inova/kotlinova/navigation/kmmsample/MainViewController.kt
+++ navigation/sample/multiplatform/composeApp/src/iosMain/kotlin/si/inova/kotlinova/navigation/kmmsample/MainViewController.kt
@@ -1,5 +1,16 @@
 package si.inova.kotlinova.navigation.kmmsample
 
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
+import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    val viewModelStoreOwner = remember { MyViewModelStoreOwner() }
+
+    CompositionLocalProvider(
+        LocalViewModelStoreOwner provides viewModelStoreOwner
+    ) {
+        App()
+    }
+}
