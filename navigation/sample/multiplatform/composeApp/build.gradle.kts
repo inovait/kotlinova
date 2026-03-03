49efa640b39250b5bc9de51bd9f24e105b5367fc
Matej Drobnič <matej@matejdro.com>
2026-03-03 08:25:58 +0100

    docs(navigation): update sample

--- navigation/sample/multiplatform/composeApp/build.gradle.kts
+++ navigation/sample/multiplatform/composeApp/build.gradle.kts
@@ -17,15 +17,15 @@ kotlin {
       }
    }
 
-   listOf(
-      iosArm64()
-      iosSimulatorArm64()
-   ).forEach { iosTarget ->
-      iosTarget.binaries.framework {
-         baseName = "ComposeApp"
-         isStatic = true
-      }
-   }
+    listOf(
+        iosArm64(),
+        iosSimulatorArm64(),
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
 
    sourceSets {
       androidMain.dependencies {
