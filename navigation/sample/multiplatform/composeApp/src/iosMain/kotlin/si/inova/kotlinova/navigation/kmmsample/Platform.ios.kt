14c50f3ac311c3e3baf82bb0a35d60ca4dac9ce4
Zvonimir Kemperle <zvonimir.kemperle@inova.si>
2026-03-03 14:13:42 +0100

    docs(navigation): iOS part of the multiplatform sample

--- navigation/sample/multiplatform/composeApp/src/iosMain/kotlin/si/inova/kotlinova/navigation/kmmsample/Platform.ios.kt
+++ navigation/sample/multiplatform/composeApp/src/iosMain/kotlin/si/inova/kotlinova/navigation/kmmsample/Platform.ios.kt
@@ -1,8 +1,14 @@
 package si.inova.kotlinova.navigation.kmmsample
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.DependencyGraph
+import dev.zacsweers.metro.SingleIn
+import dev.zacsweers.metro.createGraph
 import platform.UIKit.UIDevice
 import si.inova.kotlinova.navigation.di.NavigationInjection
+import si.inova.kotlinova.navigation.di.OuterNavigationScope
 
 class IOSPlatform : Platform {
    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
@@ -12,5 +18,17 @@ actual fun getPlatform(): Platform = IOSPlatform()
 
 @Composable
 actual fun loadNavigationInjectionFactory(): NavigationInjection.Factory {
-   TODO("Not yet implemented")
+   return remember {
+      DependencyInjectionHolder.appGraph.getNavigationInjectionFactory()
+   }
+}
+
+object DependencyInjectionHolder {
+   val appGraph by lazy { createGraph<iOSAppGraph>() }
+}
+
+@DependencyGraph(AppScope::class, additionalScopes = [OuterNavigationScope::class])
+@SingleIn(AppScope::class)
+interface iOSAppGraph: AppGraph {
+   override fun getNavigationInjectionFactory(): NavigationInjection.Factory
 }
