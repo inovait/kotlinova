a94b221bd9788bcda776c7f33f571c667c0050f1
Matej Drobnic <matej.drobnic@inova.si>
2026-03-12 13:56:47 +0100

    docs(navigation): demonstrate iOS ViewModelStore clear

--- navigation/sample/multiplatform/composeApp/src/iosMain/kotlin/si/inova/kotlinova/navigation/kmmsample/MainViewController.kt
+++ navigation/sample/multiplatform/composeApp/src/iosMain/kotlin/si/inova/kotlinova/navigation/kmmsample/MainViewController.kt
@@ -1,12 +1,20 @@
 package si.inova.kotlinova.navigation.kmmsample
 
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.DISPOSED
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 
 fun MainViewController() = ComposeUIViewController {
     val viewModelStoreOwner = remember { MyViewModelStoreOwner() }
+
+    DisposableEffect(viewModelStoreOwner) {
+        onDispose {
+            viewModelStoreOwner.viewModelStore.clear()
+        }
+    }
 
     CompositionLocalProvider(
         LocalViewModelStoreOwner provides viewModelStoreOwner
