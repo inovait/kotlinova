0c7b06e67283b37c39a923d3aee736ae4b4c932a
Matej Drobnic <matej.drobnic@inova.si>
2026-03-19 10:18:47 +0100

    docs(navigation): add state saving to wasm sample

--- navigation/sample/multiplatform/composeApp/src/webMain/kotlin/si/inova/kotlinova/navigation/kmmsample/Platform.web.kt
+++ navigation/sample/multiplatform/composeApp/src/webMain/kotlin/si/inova/kotlinova/navigation/kmmsample/Platform.web.kt
@@ -3,11 +3,23 @@ package si.inova.kotlinova.navigation.kmmsample
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.ContributesTo
 import dev.zacsweers.metro.DependencyGraph
+import dev.zacsweers.metro.Provides
 import dev.zacsweers.metro.SingleIn
 import dev.zacsweers.metro.createGraph
+import kotlinx.serialization.PolymorphicSerializer
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+import kotlinx.serialization.modules.subclass
 import si.inova.kotlinova.navigation.di.NavigationInjection
+import si.inova.kotlinova.navigation.di.NavigationSerializersModule
 import si.inova.kotlinova.navigation.di.OuterNavigationScope
+import si.inova.kotlinova.navigation.kmmsample.first.FirstScreenKey
+import si.inova.kotlinova.navigation.kmmsample.second.SecondScreenKey
+import si.inova.kotlinova.navigation.screenkeys.ScreenKey
+import si.inova.kotlinova.navigation.serialization.defaultNavigationSerializersModule
 
 class WebPlatform : Platform {
     override val name: String = "Web"
@@ -28,6 +40,38 @@ object DependencyInjectionHolder {
 
 @DependencyGraph(AppScope::class, additionalScopes = [OuterNavigationScope::class])
 @SingleIn(AppScope::class)
-interface WebAppGraph: AppGraph {
+interface WebAppGraph : AppGraph {
     override fun getNavigationInjectionFactory(): NavigationInjection.Factory
+
+    @NavigationSerializersModule
+    fun getNavigationSavedStateJsonSerializer(): Json
+
+
+    @NavigationSerializersModule
+    @Provides
+    @SingleIn(AppScope::class)
+    fun provideNavigationSavedStateJsonSerializer(
+        @NavigationSerializersModule
+        serializersModule: SerializersModule
+    ): Json {
+     return Json {
+         this.serializersModule = serializersModule
+     }
+    }
+}
+
+@ContributesTo(OuterNavigationScope::class)
+interface NavigationSerializationProviders {
+    @Provides
+    @NavigationSerializersModule
+    fun provideNavigationSerializers(): SerializersModule = SerializersModule {
+        include(defaultNavigationSerializersModule)
+
+        polymorphic(ScreenKey::class) {
+            subclass(FirstScreenKey::class)
+            subclass(SecondScreenKey::class)
+        }
+
+        contextual(ScreenKey::class, PolymorphicSerializer<ScreenKey>(ScreenKey::class))
+    }
 }
